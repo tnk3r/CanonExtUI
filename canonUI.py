9d00dc05f8f50a54ac9346cf353a2867faaeb4b6
tnk3r <abcfilm@gmail.com>
2017-09-03 18:50:34 -0700

    updated ui

--- canonUI.py
+++ canonUI.py
@@ -47,7 +47,8 @@ class canonController(QThread):
             self.send("c")
 
     def wideOpen(self):
-        self.send("f")
+        for i in range(4):
+            self.send("f")
         self.sliderInt.emit(1000)
         self.closer = 1000
 
@@ -58,41 +59,49 @@ class Window(QtGui.QWidget):
         self.setFixedSize(800, 480)
         self.bg = QtGui.QLabel("", self)
         self.bg.setFixedSize(800, 480)
-        self.bg.setStyleSheet("background-image: url(frank2.jpeg);")
+        self.bg.setStyleSheet("background-color: black;")
+
         self.bg2 = QtGui.QLabel("", self)
         self.bg2.setFixedSize(800, 480)
         self.bg2.setStyleSheet("border: 8px solid green;")
 
         self.openIrisButton = QtGui.QPushButton(" Open ", self)
-        self.openIrisButton.setStyleSheet(self.buttonstyle())
+        self.openIrisButton.setStyleSheet(self.buttonstyle(30))
         self.openIrisButton.move(100, 120)
 
         self.closeIrisButton = QtGui.QPushButton(" Close  ", self)
-        self.closeIrisButton.setStyleSheet(self.buttonstyle())
+        self.closeIrisButton.setStyleSheet(self.buttonstyle(30))
         self.closeIrisButton.move(300, 120)
 
-        self.wideOpenButton = QtGui.QPushButton(" Wide open ", self)
-        self.wideOpenButton.setStyleSheet(self.buttonstyle())
+        self.wideOpenButton = QtGui.QPushButton(" WFO WTF ", self)
+        self.wideOpenButton.setStyleSheet(self.buttonstyle(30))
         self.wideOpenButton.move(500, 120)
+
+        self.secret = QtGui.QPushButton(" secret ", self)
+        self.secret.setStyleSheet(self.buttonstyle(18))
+        self.secret.move(40, 430)
+
+        self.secret.clicked.connect(self.changeSecret)
 
         self.titleLabel = QtGui.QLabel("CanonEXT Kontrol", self)
         self.titleLabel.move(250, 50)
-        self.titleLabel.setStyleSheet("color: orange; font-size: 30px;")
+        self.titleLabel.setStyleSheet("color: orange; font-size: 40px;")
 
-        self.titleLabel2= QtGui.QLabel("by Alex/tinker", self)
-        self.titleLabel2.move(650, 450)
-        self.titleLabel2.setStyleSheet("color: lime; font-size: 15px;")
+        self.titleLabel2= QtGui.QLabel("by AlexCarr TinkerWorks, inc", self)
+        self.titleLabel2.move(550, 450)
+        self.titleLabel2.setStyleSheet("color: orange; font-size: 15px;")
 
         self.slider = QtGui.QSlider(self)
         self.slider.setMaximum(1000)
         self.slider.setMinimum(0)
-        self.slider.move(100, 280)
+        self.slider.move(100, 250)
         self.slider.setFixedSize(600, 150)
         self.slider.setStyleSheet(self.Sliderstylesheet())
         self.slider.sliderMoved.connect(self.setIrisFromSlider)
         self.slider.setOrientation(1)
 
         self.oldValue = 0
+        self.secrets = 0
         self.show()
         self.raise_()
 
@@ -101,6 +110,14 @@ class Window(QtGui.QWidget):
             self.assignFunctions()
         except StandardError as msg:
             print str(msg)
+
+    def changeSecret(self):
+        if self.secrets == 1:
+            self.bg.setStyleSheet("background-color: black;")
+            self.secrets = 0
+        else:
+            self.bg.setStyleSheet("background-image: url(/root/python/frank2.jpg);")
+            self.secrets = 1
 
     def assignFunctions(self):
         self.openIrisButton.clicked.connect(self.lensController.openIris)
@@ -115,7 +132,7 @@ class Window(QtGui.QWidget):
             self.lensController.closeIrisSlider()
         self.oldValue = self.slider.value()
 
-    def buttonstyle(self):
+    def buttonstyle(self, size):
         return """
                 QPushButton {
                     color: white;
@@ -124,7 +141,7 @@ class Window(QtGui.QWidget):
                     border-width: 2px;
                     border-radius: 5px;
                     border-color: white;
-                    font: bold 35px;
+                    font: bold """+str(size)+"""px;
                     font-style: italic;
                 }
                 QPushButton::pressed {
